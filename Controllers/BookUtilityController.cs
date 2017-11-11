a516d878a8f403b57453ccea164c216e07909a27
ANCHAL CHAUDHARY <anchal.ldn5@gmail.com>
2017-11-11 23:59:37 +0530

    List of halls sent in JSON fixed

---
+++ Controllers/BookUtilityController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UtilityBookingSystem.Models;
+
+namespace UtilityBookingSystem.Controllers
+{
+    public class BookUtilityController : Controller
+    {
+        // GET: BookUtility
+        Hall objHall = new Hall();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[HttpPost]
+        public JsonResult GetHallsList()
+        {
+            List<tblHall> hallsList = objHall.GetHallsList();
+            return Json(hallsList);
+        }
+    }
+}
--- Models/Hall.cs
+++ Models/Hall.cs
@@ -2,13 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using UtilityBookingSystem.Repository;
 
 namespace UtilityBookingSystem.Models
 {
     public class Hall
     {
+        #region tblHall attributes
         public int hallID { get; set; }
         public string hallName { get; set; }
         public Nullable<int> requirementID { get; set; }
+        #endregion
+        #region Repository objects
+        BookingRepository objBookingRepository = new BookingRepository();
+        #endregion
+        #region Get List of Halls
+        public List<tblHall> GetHallsList()
+        {
+            List<tblHall> hallsList;
+            using (var context = new BookingSystemDBEntities())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+               // context.Configuration.ProxyCreationEnabled = true;
+
+                hallsList =context.tblHalls.ToList();
+            }
+
+            return hallsList;
+        }
+        #endregion
     }
 }
